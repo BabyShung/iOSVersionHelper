dac92f73eba590e2bfc5bd3fc66bc49521001caa
Hao Zheng <babyshung8@gmail.com>
2015-02-25 10:44:20 -0500

    Initial Commit

---
+++ iOSVersionHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSVersionHelper
+//
+//  Created by Hao Zheng on 2/25/15.
+//  Copyright (c) 2015 Alarm.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSVersionHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSVersionHelper
+//
+//  Created by Hao Zheng on 2/25/15.
+//  Copyright (c) 2015 Alarm.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
